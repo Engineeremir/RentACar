e1973f605fa264345cb4d8ab8abbe6fd50390ca6
emirhan.kalem320 <emirhan.kalem320@gmail.com>
2023-08-09 12:39:10 +0300

    Project layes created

---
+++ src/rentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,24 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class Brand:Entity<Guid>
+{
+    public string Name { get; set; }
+
+    public Brand()
+    {
+        
+    }
+
+    public Brand(Guid id,string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
